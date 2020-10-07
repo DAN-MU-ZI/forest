5353943a4ec9b552c596597c39b1dadb6ac72937
forest <dlwogns6818@gmail.com>
2020-10-07 23:56:41 +0900

    Add Move Script
    이동하는 스크립트를 구현했지만 조작을 통해서 이동하는 기능을 구현해야함 현재는 시작과 동시에 좌표가 수정만됨

---
+++ Assets/02.Scripts/MoveScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3 (1,1,1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
